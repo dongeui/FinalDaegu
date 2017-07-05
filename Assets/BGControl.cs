5845a3b02a016388881f464b5601abd6fb6b37d9
dongeuijung <jde8993@naver.com>
2017-07-05 16:13:18 +0900

    배경 수정

---
+++ Assets/BGControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGControl : MonoBehaviour
+{
+    public float speedX = 0.1f;
+
+    // Use this for initialization
+    private void Start()
+
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //transform.Translate()
+        this.transform.Translate(new Vector3(speedX, 0, 0));
+    }
+}
